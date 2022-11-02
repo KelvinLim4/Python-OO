9626855c9c70fa0cc93c0ab4301017183d0c5d46
Kelvin <kelvinlimasouza226@gmail.com>
2022-11-02 10:55:41 -0300

    comitando funcionalidades saca, deposita e extrato

--- teste.py
+++ teste.py
@@ -2,3 +2,14 @@
 def cria_conta(numero, titular, saldo, limite):
     conta = {"nume": numero, "titular": titular, "saldo": saldo, "limite": limite}
     return conta
+
+#Função para depositar dinheiro
+def deposita(conta, valor):
+    conta ["saldo"] += valor
+
+#Função para sacar dinheiro
+def saca(conta, valor):
+    conta ["saldo"] -= valor
+
+def extrato(conta):
+    print("Saldo é {}".format(conta["saldo"]))
