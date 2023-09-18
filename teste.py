2bc8b8a6dc62f47faf1bb0005b40bf886e22db88
kelvin <kelvinlimasouza226@gmail.com>
2023-09-18 11:23:27 -0300

    Criando conta

--- teste.py
+++ teste.py
@@ -1,15 +1,3 @@
-
 def cria_conta(numero, titular, saldo, limite):
-    conta = {"nume": numero, "titular": titular, "saldo": saldo, "limite": limite}
+    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite }
     return conta
-
-#Função para depositar dinheiro
-def deposita(conta, valor):
-    conta ["saldo"] += valor
-
-#Função para sacar dinheiro
-def saca(conta, valor):
-    conta ["saldo"] -= valor
-
-def extrato(conta):
-    print("Saldo é {}".format(conta["saldo"]))
